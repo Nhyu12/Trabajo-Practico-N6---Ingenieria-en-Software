a19a82bfc7a79ad3797cdc7638ae7dd0f378cda2
KiiroiSe <85713780+KiiroiSe@users.noreply.github.com>
2021-10-13 18:40:37 -0300

    alo
    oliii

--- src/tp6ingsoftware/Producto.java
+++ src/tp6ingsoftware/Producto.java
@@ -8,20 +8,56 @@ package tp6ingsoftware;
  *
  * @author Usuario
  */
-public class Producto extends TipoProducto {
+public class Producto {
+    private String nombreProducto;
+    private String descripcionProducto;
+    private int stockDisponible;
+    private Double precioProducto;
     private long codigo;
     private Carrito carritoAsociado;
 
-    public Producto(long codigo, Carrito carritoAsociado, String nombreProducto, String descripcionProducto, int stockDisponible) {
-        super(nombreProducto, descripcionProducto, stockDisponible);
+    public Producto(long codigo, String nombreProducto, String descripcionProducto, int stockDisponible, Double precioProducto) {
+        this.nombreProducto = nombreProducto;
+        this.descripcionProducto = descripcionProducto;
+        this.stockDisponible = stockDisponible;
+        this.precioProducto= precioProducto;
         this.codigo = codigo;
-        this.carritoAsociado = carritoAsociado;
     }
 
-    public Producto(String nombreProducto, String descripcionProducto, int stockDisponible) {
-        super(nombreProducto, descripcionProducto, stockDisponible);
+    public Producto() {
     }
 
+    public String getDescripcionProducto() {
+        return descripcionProducto;
+    }
+
+    public void setDescripcionProducto(String descripcionProducto) {
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    public int getStockDisponible() {
+        return stockDisponible;
+    }
+
+    public void setStockDisponible(int stockDisponible) {
+        this.stockDisponible = stockDisponible;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public Double getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(Double precioProducto) {
+        this.precioProducto = precioProducto;
+    }
     public long getCodigo() {
         return codigo;
     }
@@ -37,10 +73,6 @@ public class Producto extends TipoProducto {
     public void setCarritoAsociado(Carrito carritoAsociado) {
         this.carritoAsociado = carritoAsociado;
     }
-    
-    
-    
-    
     
     
 }
--- test/TestUnitarios.java
+++ test/TestUnitarios.java
@@ -10,6 +10,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import tp6ingsoftware.Carrito;
+import tp6ingsoftware.Producto;
 
 /**
  *
@@ -34,11 +36,48 @@ public class TestUnitarios {
     
     @After
     public void tearDown() {
+    } 
+    //Verificar que el precio total (total de productos + impuestos + envio) sea igual al calculado. Para ello, tomaremos los siguientes datos
+    //Nombre: "Procesador Ryzen 3"
+    //Codigo: 25568745
+    //Descripcion: "descripcion prod"
+    //Stock:25
+    //Precio: 250.0
+    //Inpuestos: 65%. Me da un total de: 487.5
+    //Envio: Seleccionamos envio a domicilio, en una provincia fuera de Mendoza (Concretamente Buenos Aires), por lo que el envio total es de 600.0
+    //TOTAL DE LA COMPRA(PRODUCTOS+ENVIO+IMPUESTOS)=1837.5
+    @Test
+    public void ValidarSumaTotalMasEnvio(){
+        Producto p1 = new Producto(255621579,"Procesador Ryzen 3","descripcion prod",25,250.0);
+        Producto p2 = new Producto(255621579,"Procesador Ryzen 3","descripcion prod",25,250.0);
+        Producto p3 = new Producto(255621579,"Procesador Ryzen 3","descripcion prod",25,250.0);
+        Carrito c = new Carrito();
+        c.añadirProducto(p1);
+        c.añadirProducto(p2);
+        c.añadirProducto(p3);
+        c.seleccionarMetodoDeEnvio('a',"buenos aires");
+        assertEquals(c.totalCarrito(),1837.5,0);
+        
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    
+    //Verificar que el precio total (total de productos + impuestos) sea igual al calculado. Para ello, tomaremos los siguientes datos
+    //Nombre: "Procesador Ryzen 3"
+    //Codigo: 25568745
+    //Descripcion: "descripcion prod"
+    //Stock:25
+    //Precio: 250.0
+    //Inpuestos: 65%. Me da un total de: 487.5
+    //Suma Total (productos+impuestos)=1237.5
+    @Test
+    public void validarSumaTotal(){
+        Producto p1 = new Producto(255621579,"Procesador Ryzen 3","descripcion prod",25,250.0);
+        Producto p2 = new Producto(255621579,"Procesador Ryzen 3","descripcion prod",25,250.0);
+        Producto p3 = new Producto(255621579,"Procesador Ryzen 3","descripcion prod",25,250.0);
+        Carrito c = new Carrito();
+        c.añadirProducto(p1);
+        c.añadirProducto(p2);
+        c.añadirProducto(p3);
+        assertEquals(c.sumaTotal(),1237.5,0);
+    }
 }
